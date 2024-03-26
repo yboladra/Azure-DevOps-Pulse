c3b858a083d825a5e0829f7cbcc05ecd64e1efd9
Hriyen Tripura <128646718+HrIyEn07@users.noreply.github.com>
2024-03-26 17:10:03 +0530

    Update Virtual_Network.tf

--- WebApp-ASP.Net core/terraform-manifests/Virtual_Network.tf
+++ WebApp-ASP.Net core/terraform-manifests/Virtual_Network.tf
@@ -1,6 +1,6 @@
 # Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "cos-vnet01"
+  name                = "cos-vnet02"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.HriyenRG.location
   resource_group_name = azurerm_resource_group.HriyenRG.name
