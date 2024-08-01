23a0c4453b70c1d9ba04ffc7c0d0d4ca8fa507ef
Hriyen <hriyen.tripura@gmail.com>
2024-08-01 16:43:24 +0530

    backend starage added

--- WebApp-ASP.Net core/terraform-manifests/backend.tf
+++ WebApp-ASP.Net core/terraform-manifests/backend.tf
@@ -8,8 +8,8 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name = "terraform_rg01"
-    storage_account_name = "terraformstate2015"
+    resource_group_name = "terraform-rg10"
+    storage_account_name = "terraformbackend0001"
     container_name = "tfcontainer01"
     key = "terraform.tfstate"
   }
